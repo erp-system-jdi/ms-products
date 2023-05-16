c81570df4878c1cdf294a3a9f14a665ac7689573
Giovanni Marchi <giovanni.marchi92@gmail.com>
2023-05-16 17:54:56 -0300

    changes to up

--- src/main/java/br/com/erpsystem/msproduct/MsProductApplication.java
+++ src/main/java/br/com/erpsystem/msproduct/MsProductApplication.java
@@ -1,9 +1,13 @@
 package br.com.erpsystem.msproduct;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.amqp.RabbitAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = RabbitAutoConfiguration.class)
 public class MsProductApplication {
 
 	public static void main(String[] args) {
